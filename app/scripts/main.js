20deeb36e9027f510235f042c56222266b383cf8
Jan Klever <janklever@gmail.com>
2017-01-22 19:37:32 -0300

    gitignore updated and script created for links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+console.log('Jan Klever');
+
+$('a').on('mouseenter', function () {
+    var attr = $(this).attr('id');
+    $('#icon').html(attr);
+    $('body').addClass('hover-' + attr);
+});
+
+$('a').on('mouseout', function () {
+    var attr = $(this).attr('id');
+    $('body').removeClass().fadeIn();
+});
